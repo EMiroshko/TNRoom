1dc5ad729d6d7e3d3b8cd2e8491b06ef7a3636a6
emiroshko <miro6ko@gmail.com>
2019-04-03 14:43:25 +0300

    Updates

--- vendors.bundle.21697588b86ec81e68b7.js
+++ vendors.bundle.21697588b86ec81e68b7.js
@@ -91,7 +91,7 @@
 /******/ 		if (__webpack_require__.nc) {
 /******/ 			script.setAttribute("nonce", __webpack_require__.nc);
 /******/ 		}
-/******/ 		script.src = __webpack_require__.p + "" + chunkId + ".bundle." + {"0":"6a3463b3d30dc6d0f20a"}[chunkId] + ".js";
+/******/ 		script.src = __webpack_require__.p + "" + chunkId + ".bundle." + {"0":"8b7cbb1f58c40d5c3b39"}[chunkId] + ".js";
 /******/ 		var timeout = setTimeout(onScriptComplete, 120000);
 /******/ 		script.onerror = script.onload = onScriptComplete;
 /******/ 		function onScriptComplete() {
